b901b43d6631f77fe6da32400d5081a6410fb959
Pittab <120305267+Pittab@users.noreply.github.com>
2024-03-08 11:23:54 +0000

    Update innovation.js

--- innovation.js
+++ innovation.js
@@ -1,5 +1,14 @@
-let ino=1
-function innovation() {
-    alert("innovation counter is at: " + ino + " innovations")
-    ino += 1
-}
+let ino=0
+
+function innovation() 
+{
+    ino += 1;
+    alert("innovation counter is at: " + ino + " innovations");
+     }
+
+
+function unin() 
+{
+     ino -= 1;
+     alert("you uninnovated. innovation counter is at: " + ino + " innovations"); 
+     }
